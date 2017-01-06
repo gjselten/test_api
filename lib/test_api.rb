197d7d1fbb8ada7114456554d9d3c19624370907
gjselten <geert-jan@cg.nl>
2017-01-06 10:54:42 +0100

    first commit

---
+++ lib/test_api.rb
@@ -0,0 +1,10 @@
+require "test_api/version"
+
+module TestApi
+  # Your code goes here...
+  class Client
+    def test_puts
+      puts 'this is a test'
+    end
+  end
+end
---
+++ lib/test_api/version.rb
@@ -0,0 +1,3 @@
+module TestApi
+  VERSION = "0.1.0"
+end
